7d61fd8b4ae08dfd031921a3ac1ecbf02967d473
Andrian.Mamchuk <andrian.mamchuk@gmail.com>
2023-10-15 00:50:57 +0300

    Ready goit-js-hw-01

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding) * 2;
-  const borderWidth = Number.parseFloat(border) * 2;
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding) * 2;
+  const borderWidth = parseFloat(border) * 2;
 
-  return (contentWidth + paddingWidth + borderWidth).toFixed(0);
+  return contentWidth + paddingWidth + borderWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
